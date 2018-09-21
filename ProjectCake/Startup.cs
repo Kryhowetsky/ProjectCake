e61ed618e1a69ac717e7ea7e43b1baed47540755
kryhowetsky <kryhowetsky25@gmail.com>
2018-09-21 16:04:38 +0300

    Change MapRoute to main page cakes

--- ProjectCake/Startup.cs
+++ ProjectCake/Startup.cs
@@ -59,7 +59,7 @@ namespace ProjectCake
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Product}/{action=Index}/{id?}");
             });
 
             var serviceProvider = app.ApplicationServices;
